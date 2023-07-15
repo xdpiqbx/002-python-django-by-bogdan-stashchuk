381ef19ab7c198ebe760ace73bb375c12b8ca00b
xdpiqbx <xdpiqbx@gmail.com>
2023-07-15 23:11:30 +0300

    refactor html

--- base/urls.py
+++ base/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls')),
+    # path('shop/', include('shop.urls')),
+    path('', include('shop.urls')),
     path('api/', include('api.urls')),
 ]
